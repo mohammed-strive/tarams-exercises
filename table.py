8a5fc91d10c9f161a3961b6a6bc4522e016bfffb
S M Jaffer <45582402+mohammed-strive@users.noreply.github.com>
2020-06-05 11:42:13 +0530

    Update table.py
    Refactoring the code.

--- table.py
+++ table.py
@@ -1,14 +1,64 @@
 import sys
+import regex as re
 
-def extract_table(rawData, noColumns):
-    rawData = list(filter(bool, map(str.strip, rawData)))
-    data = {} 
-    for i in range(noColumns):
-        slice = rawData[i::noColumns]
-        data[slice[0]] = slice[1:]
+# Add patterns to this hash..
+# The patterns will be regex Objects that match specific items in the given
+# string.
+patterns = {
+        'Date of Joining':
+        re.compile(r'(?<date>\d{2,4}[/\\-]+?\d{2,4}[\\/-]\d{2,4})', re.A | re.I),
+        'Id': re.compile(r'(?<id>\d+)'),
+        'Name': re.compile(r'(?<name>[A-Za-z,]+)'),
+        }
+
+# This function will run the regex in the patterns object for each line and
+# return the matches.
+def parse_line(line, patterns):
+
+    for key, regex in patterns.items():
+        match = regex.match(line)
+        if match:
+            return key, match
+
+    return None, None
+
+
+# The meat of the parsing logic. Grab each line and run the pattern match.
+# Upon receving any match object validate the items.
+def parse_table(filename):
+
+    data = []
+
+    with open(filename, 'r') as fh:
+        for line in fh:
+            items = 0
+            row = []
+            while items < 3:
+                line  = line.strip()
+                if line :
+                    key, match = parse_line(line, patterns)
+                    if key == 'Date of Joining':
+                        row.append(match.group('date'))
+                    elif key == 'Id':
+                        row.append(match.group('id'))
+                    elif key == 'Name':
+                        row.append(match.group('name'))
+
+                    items += 1
+                    line = fh.readline()
+                else:
+                    line = fh.readline()
+                    continue
+            data.append(row)
 
     return data
 
-with open(sys.argv[1], 'r') as fh:
-    rawData = fh.readlines()
-    print(extract_table(rawData, 3))
+if __name__ == '__main__':
+    try:
+        filename = sys.argv[1]
+        print(parse_table(filename))
+    except IndexError as e:
+        print("Filename argument not provided")
+        print("Invocation -- ")
+        print("python3 {} path/to/file".format(__file__))
+        sys.exit(1)
